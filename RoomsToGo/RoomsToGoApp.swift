66ae8db6fbc85eef061ad8db4777b2ad2152cbd2
Richard Rubin <iOSSoftwareEngineer@users.noreply.github.com>
2023-05-08 14:29:34 -0400

    Initial Commit

---
+++ RoomsToGo/RoomsToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsToGoApp.swift
+//  RoomsToGo
+//
+//  Created by Richard B. Rubin on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RoomsToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
